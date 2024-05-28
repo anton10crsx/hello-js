89f608f41d7a1f3a1e93a0dfac77ffd4a720c32c
Anton X <anton10.crsx@gmail.com>
2024-05-28 11:11:58 +0200

    coffee...

---
+++ section4/lesson10/script.js
@@ -0,0 +1,17 @@
+console.log("Hello...");
+
+console.log(Math.PI);
+// *
+console.log(Math.floor(3.3));
+console.log(Math.ceil(3.3));
+
+// Random
+
+// random() 0 to 1
+console.log(Math.random());
+
+// random()*10 0 to 9
+console.log(Math.random() * 10);
+// Dice
+console.log("%cDice =", "color: green; font-size: 24;");
+console.log(Math.floor(Math.random() * 6) + 1);
--- section4/lesson9/script.js
+++ section4/lesson9/script.js
@@ -1,1 +1,24 @@
 console.log("%cHappy Coding!", "color: red; font-size: 24px;");
+
+let box = document.querySelector("#box");
+// console.log(box);
+
+/*
+// e - event
+box.onclick = function (event) {
+  // console.log(event);
+  console.log(event.target);
+  box.innerHTML = +box.innerHTML + 1; // + Number
+};
+*/
+
+box.onclick = counter;
+
+function counter(e) {
+  console.log(e.target);
+  box.innerHTML = +box.innerHTML + 1;
+}
+
+// turbo-console
+let xxx = 9;
+console.log("🚀 ~ xxx:", xxx);
