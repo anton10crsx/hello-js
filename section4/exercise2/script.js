972bbfcda4345797d55a66ed55c30118228a2eb5
Anton X <anton10.crsx@gmail.com>
2024-05-28 13:08:02 +0200

    ex2 v...

--- section4/exercise2/script.js
+++ section4/exercise2/script.js
@@ -1,1 +1,64 @@
-console.log("Hi!");
+console.log("=== === ===");
+
+let ptRoma;
+let ptNapoli;
+let ptBologna;
+let ptTot;
+
+ptRoma = document.querySelector("#roma td:nth-of-type(2)");
+ptNapoli = document.querySelector("#napoli td:nth-of-type(2)");
+ptBologna = document.querySelector("#bologna td:nth-of-type(2)");
+ptTot = document.querySelector("#tot th:nth-of-type(2");
+
+// Roma
+ptRoma.onclick = function () {
+  let oldPtRoma;
+  let newPtRandom;
+  let newPtTot;
+  let otherClubs;
+
+  oldPtRoma = +ptRoma.innerHTML; // Number
+  newPtRandom = Math.floor(Math.random() * 100) + 1;
+
+  // ptRoma.innerHTML = newPtRandom;
+
+  newPtTot = +ptTot.innerHTML;
+  otherClubs = newPtTot - oldPtRoma;
+
+  ptRoma.innerHTML = newPtRandom;
+  ptTot.innerHTML = otherClubs + newPtRandom;
+};
+
+// Napoli
+ptNapoli.onclick = function () {
+  let oldPtNapoli;
+  let newPtRandom;
+  let newPtTot;
+  let otherClubs;
+
+  oldPtNapoli = +ptNapoli.innerHTML; // Number
+  newPtRandom = Math.floor(Math.random() * 100) + 1;
+
+  newPtTot = +ptTot.innerHTML;
+  otherClubs = newPtTot - oldPtNapoli;
+
+  ptNapoli.innerHTML = newPtRandom;
+  ptTot.innerHTML = otherClubs + newPtRandom;
+};
+
+// Bologna
+ptBologna.onclick = function () {
+  let oldPtBologna;
+  let newPtRandom;
+  let newPtTot;
+  let otherClubs;
+
+  oldPtBologna = +ptBologna.innerHTML; // Number
+  newPtRandom = Math.floor(Math.random() * 100) + 1;
+
+  newPtTot = +ptTot.innerHTML;
+  otherClubs = newPtTot - oldPtBologna;
+
+  ptBologna.innerHTML = newPtRandom;
+  ptTot.innerHTML = otherClubs + newPtRandom;
+};
