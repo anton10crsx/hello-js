e7aab6a30799d933ae759159cb9890777c739d8c
Anton X <anton10.crsx@gmail.com>
2024-05-30 09:14:29 +0200

    add lesson

--- section4/lesson12/script.js
+++ section4/lesson12/script.js
@@ -1,9 +1,27 @@
 console.log("===");
 
 let inputText = document.querySelector(".input-text");
+let pickerText = document.querySelector(".picker-text");
+let selectCity = document.querySelector("#select-city");
 
-inputText.addEventListener("change", inputHandler);
+// e_change
+// inputText.addEventListener("change", inputHandler);
+
+// function inputHandler() {
+//   console.log("Event Fired!"); // ...
+//   pickerText.textContent = inputText.value;
+// }
+
+// e_input
+inputText.addEventListener("input", inputHandler);
 
 function inputHandler() {
   console.log("Event Fired!"); // ...
+  pickerText.textContent = inputText.value;
+}
+
+selectCity.addEventListener("change", selectHandler);
+
+function selectHandler() {
+  console.log(selectCity.value);
 }
